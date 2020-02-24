7998688c6653f1a1e7867185d7d570258c75af31
murilo <murilofmadureira@hotmail.com>
2020-02-24 12:13:30 -0300

    correção

--- produtor-service/src/main/java/br/com/loja/produtor/amq/source/VendaCodSource.java
+++ produtor-service/src/main/java/br/com/loja/produtor/amq/source/VendaCodSource.java
@@ -1,10 +1,10 @@
 package br.com.loja.produtor.amq.source;
 
 import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
 
 public interface VendaCodSource {
 
     @Input(Channels.VENDA_COD_CHANNEL)
-    MessageChannel inputChannel();
+    SubscribableChannel inputChannel();
 }
